975fc6bc480a6572f3876179779bcc729afbbf86
yangfan <982465979@qq.com>
2022-06-30 15:48:41 +0800

    添加个注释

--- vue3-vite/src/autoImportUtils/index.js
+++ vue3-vite/src/autoImportUtils/index.js
@@ -3,6 +3,7 @@ import modal from '../plugins/modal';
 import api from '../api/api';
 import notify from '@zh-or/notify-js';
 
+//这里导出的可以在vue的steup里面直接使用
 export const $t = t;
 export const $modal = modal;
 export const $api = api;
