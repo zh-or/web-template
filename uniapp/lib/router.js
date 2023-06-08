074e864fe1a1c5360d126bf77b6c166e6b07a028
yangfan <982465979@qq.com>
2023-06-08 17:17:46 +0800

    none

--- uniapp/lib/router.js
+++ uniapp/lib/router.js
@@ -1,20 +1,24 @@
+import tools from './tools';
+
 export default {
     paths: [],
     animation: 'none',
     resultSuccess: null,
     resultError: null,
     currentPage() {
+        //return this.paths[this.paths.length - 1];
+
         let pages = getCurrentPages();
         if(pages.length > 0) {
             return '/' + pages[pages.length - 1].route;
         }
         return '';
     },
-    pushForResult(opt) {
+    pushForResult(opt, params) {
         return new Promise((resolve, reject) => {
             this.resultSuccess = resolve;
             this.resultError = reject;
-            this.push(opt);
+            this.push(opt, params);
         });
     },
     setResult(data, back = true) {
@@ -31,7 +35,7 @@ export default {
      * 保留当前页面，跳转到应用内的某个页面
      * @param opt
      */
-    push(opt) {
+    push(opt, params) {
         let obj = {
             animationType: this.animation
         };
@@ -43,37 +47,60 @@ export default {
         obj.fail = function(e) {
             console.error('router.push', e);
         }
-        if(this.currentPage() === obj.url) {
+        let currentUrl = this.currentPage();
+        console.log('current:', currentUrl === obj.url, currentUrl, obj.url);
+
+        if(currentUrl === obj.url) {
             obj.fail('重复跳转:' + obj.url);
             return;
         }
 
+        if(params && typeof params === 'object') {
+            let parStr = tools.objToQueryString(params);
+            if(obj.url.indexOf('?') != -1) {
+                obj.url += ('&' + parStr);
+            } else {
+                obj.url += ('?' + parStr);
+            }
+        }
+
         this.paths.push(obj.url);
 
-/*
-        console.log(
-            this.currentPage(),
-            obj.url,
-            this.currentPage() === obj.url,
-            this.paths
-        );*/
+        /*
+                console.log(
+                    this.currentPage(),
+                    obj.url,
+                    this.currentPage() === obj.url,
+                    this.paths
+                );*/
 
-		// console.log(obj);
         uni.navigateTo(obj);
     },
     /**
      * 关闭当前页面，跳转到应用内的某个页面。
      * @param url
      */
-    replace(url) {
+    replace(url, animationType) {
         let len = this.paths.length;
         if (len > 0) {
             this.paths[len - 1] = url;
         } else {
             this.paths.push(url);
         }
+
+        if(animationType && typeof animationType === 'object') {
+            let parStr = tools.objToQueryString(animationType);
+            if(url.indexOf('?') != -1) {
+                url += ('&' + parStr);
+            } else {
+                url += ('?' + parStr);
+            }
+            animationType = '';
+        }
+
         uni.redirectTo({
             url: url,
+            animationType: animationType || 'none',
             fail : function(e) {
                 console.error('router.replace', e);
             }
@@ -93,6 +120,11 @@ export default {
             animationType: this.animation
         });
     },
+    delayBack(time) {
+        setTimeout(_ => {
+            this.back()
+        }, time || 1000);
+    },
     /**
      * 跳转到 tabBar 页面，并关闭其他所有非 tabBar 页面。
      * @param path 需要跳转的 tabBar 页面的路径（需在 pages.json 的 tabBar 字段定义的页面），路径后不能带参数
@@ -102,8 +134,8 @@ export default {
         uni.switchTab({
             url: path,
             fail : function(e) {
-            console.error('router.switchTab', e);
-        }
+                console.error('router.switchTab', e);
+            }
         });
     },
     /**
