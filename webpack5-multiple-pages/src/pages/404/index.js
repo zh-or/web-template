64d642e1e5ba0c3e215ef11cd03ef382cc69ed63
yangfan <982465979@qq.com>
2023-10-24 10:37:26 +0800

    防抖节流

--- webpack5-multiple-pages/src/pages/404/index.js
+++ webpack5-multiple-pages/src/pages/404/index.js
@@ -1,2 +1,30 @@
 import './index.less';
 
+import t from '../../../../lib/index.js';
+
+async function test() {
+    //防抖节流测试
+    /*let obj;
+    for(let i = 0; i < 100; i ++) {
+        obj = t.throttle(obj, function() {
+            console.log('i:' + i, new Date().toLocaleString())
+        }, 1000, true)
+
+        await t.sleep(100);
+    }
+*/
+
+    let obj2;
+    let last = Date.now();
+    for(let i = 0; i < 100; i ++) {
+        obj2 = t.debounce(obj2, function() {
+            console.log('i:' + i, Date.now() - last);
+            last = Date.now();
+        }, 500)
+
+        await t.sleep(10);
+    }
+
+}
+
+test();
